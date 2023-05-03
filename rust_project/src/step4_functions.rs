08e2cdd75b1a4362fb0f327617e4d186dd09c52f
Johannyjm <meetpastarts@gmail.com>
2023-05-03 19:13:12 +0900

    [mod] step4

--- rust_project/src/step4_functions.rs
+++ rust_project/src/step4_functions.rs
@@ -1,7 +1,15 @@
 pub fn run() {
     greeting("Hello", "John");
+
+    let sum = add(10, 20);
+
+    println!("10 + 20 = {}", sum);
 }
 
 fn greeting(greet: &str, name: &str) {
     println!("{} {}, nice to meet you!", greet, name);
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
