272df16490879a60538226e4c396f7c161a7d869
Johannyjm <meetpastarts@gmail.com>
2023-05-03 17:54:46 +0900

    Step3: Loop Execution

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -1,6 +1,8 @@
 mod step1_variables_and_datatypes;
 mod step2_conditions;
+mod step3_loop_execution;
 fn main() {
     step1_variables_and_datatypes::run();
     step2_conditions::run();
+    step3_loop_execution::run();
 }
---
+++ rust_project/src/step3_loop_execution.rs
@@ -0,0 +1,13 @@
+pub fn run(){
+    // for loop
+    for i in 1..=5{
+        println!("i = {}", i);
+    }
+
+    // while loop
+    let mut counter = 1;
+    while counter <= 5 {
+        println!("counte = {}", counter);
+        counter += 1
+    }
+}
