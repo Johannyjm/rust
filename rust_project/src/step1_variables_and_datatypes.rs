824cb3a71fef6553aa26017c0609d48497a4f45d
Johannyjm <meetpastarts@gmail.com>
2023-05-03 17:48:10 +0900

    [mod] chane architecture

---
+++ rust_project/src/step1_variables_and_datatypes.rs
@@ -0,0 +1,15 @@
+pub fn run() {
+    let x = 42;
+    let y: u32 = 100;
+    let z: f64 = 3.14;
+    let is_rust_fun: bool = true;
+    let ch: char = 'A';
+    let s: String = String::from("Hello, world!");
+
+    println!("x = {}", x);
+    println!("y = {}", y);
+    println!("z = {}", z);
+    println!("is_rust_fun = {}", is_rust_fun);
+    println!("ch = {}", ch);
+    println!("s = {}", s)
+}
