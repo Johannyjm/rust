702b7263332b29b8c74b8746015098a9655a8795
Johannyjm <meetpastarts@gmail.com>
2023-05-03 17:50:58 +0900

    Step2: Conditions

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -1,4 +1,6 @@
 mod step1_variables_and_datatypes;
+mod step2_conditions;
 fn main() {
     step1_variables_and_datatypes::run();
+    step2_conditions::run();
 }
---
+++ rust_project/src/step2_conditions.rs
@@ -0,0 +1,13 @@
+pub fn run(){
+    let number = 10;
+
+    if number < 10 {
+        println!("The number is less than 10");
+    }
+    else if number > 10 {
+        println!("The number is greater than 10");
+    }
+    else{
+        println!("The number is 10")
+    }
+}
